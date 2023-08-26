50f4fe73a2397098ea63902f953b103f3f8ec9ac
Denis Khudyakov <denis.hudyakov27@gmail.com>
2023-08-26 23:05:12 +0500

    home_work_done м 2

--- module_3.py
+++ module_3.py
@@ -14,8 +14,8 @@ def contest(question: str, answer: str) -> bool:
       print('Ответ верный!')
       return True
 
-print("""Привет! 
-Предлагаю проверить свои знания английского! 
+print("""Привет!
+Предлагаю проверить свои знания английского!
 Наберите "ready", чтобы начать!""")
 command = input()
 if command == "ready":
@@ -32,6 +32,37 @@ if command == "ready":
                 continue
         else:
             print(f'Увы, но нет. Верный ответ: {answers[index]}')
+            continue
+    print(f"""
+Вот и всё!
+Вы ответили на {correct_answer_counter} вопросов из 3 верно.
+Это {round((correct_answer_counter / 3) * 100)} процентов.
+""")
+else:
+    print('Кажется, вы не хотите играть. Очень жаль.')
+
+# альтернативное решение c вложенным списком
+
+newList = [["My name ___  Vova ", "is"], ["I ___ a coder ", "am"], ["I live ___ Moscow ", "in"]]
+print("""Привет! 
+Предлагаю проверить свои знания английского! 
+Наберите "ready", чтобы начать!""")
+command = input()
+if command == "ready":
+    for index, value in enumerate(newList):
+        errors = 0
+        while errors != 3:
+            question_and_answer = input(value[0]).lower()
+            if question_and_answer == value[1]:
+                correct_answer_counter += 1
+                print('Ответ верный!')
+                break
+            else:
+                errors += 1
+                print(f'Осталось попыток {3 - errors}, Попробуйте ещё раз!')
+                continue
+        else:
+            print(f'Увы, но нет. Верный ответ: {value[1]}')
             continue
     print(f"""
 Вот и всё!
