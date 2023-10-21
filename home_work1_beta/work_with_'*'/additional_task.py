821bb091ab73503e8b58dd1f0898d5c2b5d0d304
deniskhudyakov <denis.hudyakov27@gmail.com>
2023-10-21 20:50:55 +0500

    home_work1_beta done

--- home_work1_beta/work_with_'*'/additional_task.py
+++ home_work1_beta/work_with_'*'/additional_task.py
@@ -1,8 +1,26 @@
 import os
 
-def count_files_and_dirs(path: str, is_walk=False) -> dict:
+
+
+def count_files_and_dirs(any_path: str, is_walk=False) -> dict:
+
+"""
+Функция, которая считает количество файлов и папок с помощью рекурсии и лист компрехеншен
+"""
+    data_dict = {"files": None, "folders": None}
+
     if is_walk:
-        for root, dir, files in os.walk(path):
-            pass
+        data_dict["files"] = len([f if os.path.isfile(os.path.join(any_path, f))
+                 else count_files_and_dirs(os.path.join(any_path, f))
+                 for f in os.listdir(any_path)])
+        data_dict["folders"] = len([f for f in os.listdir(any_path)
+                if os.path.isdir(os.path.join(any_path, f))])
+        return data_dict
+    data_dict["files"] = len([f for f in os.listdir(any_path) if os.path.isfile(os.path.join(any_path, f))])
+    data_dict["folders"] = len([f for f in os.listdir(any_path)
+                                if os.path.isdir(os.path.join(any_path, f))])
+    return data_dict
 
+print(count_files_and_dirs('/home/deniskhudyakov/PycharmProjects/SkyPro_homework/home_work1_beta', True))
 
+print(count_files_and_dirs(os.getcwd()))
